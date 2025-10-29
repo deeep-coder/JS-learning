e8de856d8f086fc2feef75cbf5cd871d363ce0a0
Deep Detroja <deepdetroja1223@gmail.com>
2025-10-29 22:49:25 +0530

    operators

---
+++ L-7/index.js
@@ -0,0 +1,38 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2");  // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32     //*** special case dont need to study in deatil it is part/ rules in ecmascript
+
+// console.log( (3 + 4) * 5 % 3);    //bad practice
+
+// console.log(+true);  // shows 1
+// console.log(true+);  // throw an error
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2   //bad syntext
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study prefix and postfix increment
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
