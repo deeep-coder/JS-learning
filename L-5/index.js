c44ba27c027c0c9ff0a16c6e909117a803e3ecdb
Deep Detroja <deepdetroja1223@gmail.com>
2025-10-29 22:12:44 +0530

    Datatypes

---
+++ L-5/index.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh" //string
+let age = 18    //number
+let isLoggedIn = false //boolean
+let state; // undefined
+let ans =null;  // null 
+
+
+// Data types
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value means khali also we can say that abhi value aayi hhi nahi hai
+// undefined => value assign nahi ki hai
+// symbol => for finding uniqueness which is mainly used in react
+
+
+// object
+
+console.log(typeof isLoggedIn);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // null ek object hai so it will shows /* object */ 
