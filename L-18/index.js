527a6fb725481078b8da1781b80062c2ad59b3e1
Deep Detroja <deepdetroja1223@gmail.com>
2025-10-31 13:36:51 +0530

    JSON API

---
+++ L-18/index.js
@@ -0,0 +1,38 @@
+
+
+
+//destructure of object means {}
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor //to access the courseInstructor value
+
+const {courseInstructor} = course // this is  another methode to access the courseInstructor value 
+// console.log(courseInstructor);
+
+
+//we can also use this methode ,here we replace the name of courseInstructor to instructor 
+const {courseInstructor: instructor} = course
+console.log(instructor);
+
+
+//*********************************************** API ***********************************************
+
+// JSON -in JSON key and vlaue both are in string
+
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
