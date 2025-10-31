25f9deaa96eb20b7ef27c71072c5050ec735fd36
Deep Detroja <deepdetroja1223@gmail.com>
2025-10-31 15:01:13 +0530

    scope and Hoisting

---
+++ L-22/index.js
@@ -0,0 +1,42 @@
+function one(){  //nested scope
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))  //logs 6
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)   //throw an error due to hoisting
+const addTwo = function(num){
+    return num + 2
+}
