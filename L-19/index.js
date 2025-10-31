fffcb61159f69a9b54a13de7833165e2b4a908c7
Deep Detroja <deepdetroja1223@gmail.com>
2025-10-31 14:21:39 +0530

    functions and parameters

---
+++ L-19/index.js
@@ -0,0 +1,43 @@
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName  //function ka refrence
+// sayMyName() //function ka execution
+
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){  //we can also add default value "Sam" if in input we give Hitesh then it will overwrite it and otherwise sam will be treat as username
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
