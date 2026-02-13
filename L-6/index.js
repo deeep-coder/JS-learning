4c6fd13e733e3650d55689929dc4fd4233c3918f
Deep Detroja <deepdetroja1223@gmail.com>
2026-02-13 20:31:20 +0530

    L-6 done

--- L-6/index.js
+++ L-6/index.js
@@ -8,7 +8,7 @@ let convertStrtoNumber= Number(score); // convert string in to number
 console.log(convertStrtoNumber); // If we give only nubers in string like "33" then it will convert it to number , but if we gave string like "33abc" then it will thraw an "NaN" means not a number
 console.log(typeof convertStrtoNumber); // in both cases like "33" and "33abc" it will return  "number" 
 
-// note :JS always string ko always number me convert kar denga so we have to carefull about it ,because this is a limitation of JS
+// ** note : JS always string ko always number me convert kar denga so we have to carefull about it ,because this is a limitation of JS
 
 
 //      <-------------------------------//conversion in boolean //-------------------------------------------------------------------------------->
